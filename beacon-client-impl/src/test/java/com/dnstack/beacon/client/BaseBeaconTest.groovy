2a699fd7b08cc4d6894a5148ded0841c30669ed4
artemik <temanovikov@gmail.com>
2016-06-28 18:21:21 +0400

    impl and tests, readme

---
+++ beacon-client-impl/src/test/java/com/dnstack/beacon/client/BaseBeaconTest.groovy
@@ -0,0 +1,79 @@
+package com.dnstack.beacon.client
+
+import com.dnastack.beacon.client.BeaconClientImpl
+import com.github.tomakehurst.wiremock.WireMockServer
+import org.apache.commons.lang.StringUtils
+import org.testng.annotations.AfterMethod
+import org.testng.annotations.AfterSuite
+import org.testng.annotations.BeforeSuite
+import org.testng.annotations.Test
+
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
+
+/**
+ * Helper class for Beacon tests.
+ * When the java property beacon.test.url is not specified, the Beacon server is mocked, otherwise tests
+ * are run against the specified Beacon (usually a real one).
+ * Not all tests might support integration testing against a real Beacon server - this is defined by
+ * {@link com.dnstack.beacon.client.BaseBeaconTest#isIntegrationTestingSupported()}
+ *
+ * @author Artem (tema.voskoboynick@gmail.com)
+ * @version 1.0
+ */
+public abstract class BaseBeaconTest {
+    static final def MOCK_BOB_PORT = 8089
+    static final def MOCK_BOB_SERVER = new WireMockServer(wireMockConfig().port(MOCK_BOB_PORT))
+    static final BeaconClientImpl CLIENT
+    static final boolean MOCKED_TESTING
+
+    /**
+     * Define if the testing will be against real Beacon Network server, or the mocked one.
+     */
+    static {
+        def beaconTestUrl = System.properties.getProperty("beacon.test.url")
+        MOCKED_TESTING = StringUtils.isBlank(beaconTestUrl)
+        CLIENT = MOCKED_TESTING ?
+                new BeaconClientImpl(new URL("http", "localhost", MOCK_BOB_PORT, "")) :
+                new BeaconClientImpl(beaconTestUrl)
+
+    }
+
+    @BeforeSuite
+    void startServer() {
+        if (MOCKED_TESTING) {
+            MOCK_BOB_SERVER.start();
+        }
+    }
+
+    @AfterSuite
+    void stopServer() {
+        if (MOCKED_TESTING) {
+            MOCK_BOB_SERVER.stop();
+        }
+    }
+
+    @AfterMethod
+    void resetMappings() {
+        if (MOCKED_TESTING) {
+            MOCK_BOB_SERVER.resetMappings();
+        }
+    }
+
+    @Test
+    void test() {
+        if (!MOCKED_TESTING && !isIntegrationTestingSupported()) {
+            return
+        }
+
+        if (MOCKED_TESTING) {
+            setupMappings()
+        }
+        doTest()
+    }
+
+    void setupMappings() {}
+
+    boolean isIntegrationTestingSupported() { return true }
+
+    abstract void doTest();
+}
---
+++ beacon-client-impl/src/test/java/com/dnstack/beacon/client/beacon/BeaconSuccessTest.groovy
@@ -0,0 +1,28 @@
+package com.dnstack.beacon.client.beacon
+
+import com.dnstack.beacon.client.BaseBeaconTest
+import com.dnstack.beacon.client.utils.Gson
+
+import static com.dnstack.beacon.client.TestData.TEST_BEACON
+import static com.github.tomakehurst.wiremock.client.WireMock.*
+import static org.assertj.core.api.Assertions.assertThat
+
+/**
+ * @author Artem (tema.voskoboynick@gmail.com)
+ * @version 1.0
+ */
+class BeaconSuccessTest extends BaseBeaconTest {
+    @Override
+    void setupMappings() {
+        MOCK_BOB_SERVER.stubFor(get(urlEqualTo("/"))
+
+                .willReturn(aResponse()
+                .withBody(Gson.toJson(TEST_BEACON))))
+    }
+
+    @Override
+    void doTest() {
+        def beacon = CLIENT.getBeacon();
+        assertThat(beacon).isEqualTo(TEST_BEACON);
+    }
+}
